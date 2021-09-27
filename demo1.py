5e75223a7cdb531e9aa9a2303a6df438335ce0c8
Glemhel <ya.rudakov2001@yandex.ru>
2021-09-27 18:50:15 +0300

    basic functions

--- demo1.py
+++ demo1.py
@@ -1,6 +1,7 @@
 from telegram.ext import MessageHandler, Filters, Updater, CommandHandler
 import logging
 import config
+import random
 
 updater = Updater(token=config.telegram_api_key)
 
@@ -8,16 +9,46 @@ dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
+# user id to list of links
+users_storage = {}
+
+
 def start(update, context):
+    print(context)
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def save_link(update, context):
+    if 'links' not in context.user_data.keys():
+        context.user_data['links'] = []
+    # user_id = update.message.from_user['id']
+    context.user_data['links'].append(context.args[0])
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Your link saved!")
+
+def get_all_links(update, context):
+    users_links = str(context.user_data['links'])
+    context.bot.send_message(chat_id=update.effective_chat.id, text=users_links)
+
+def get_random_link(update, context):
+    users_links = context.user_data['links']
+    link_name = users_links[random.randrange(0, len(users_links))]
+    context.bot.send_message(chat_id=update.effective_chat.id, text=link_name)
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+savelink_handler = CommandHandler('save', save_link)
+dispatcher.add_handler(savelink_handler)
+
+getlinks_handler = CommandHandler('get', get_all_links)
+dispatcher.add_handler(getlinks_handler)
+
+getrand_handler = CommandHandler('getrand', get_random_link)
+dispatcher.add_handler(getrand_handler)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
