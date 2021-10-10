fb62d277d7ec483aace6955d9fc6948debac55a8
Mikhail Rudakov <ya.rudakov2001@yandex.ru>
2021-10-10 16:51:13 +0300

    mongoDB connection

---
+++ storage.py
@@ -0,0 +1,55 @@
+import os
+import pymongo
+import configs
+import copy
+
+CLIENT = pymongo.MongoClient(
+    f"mongodb+srv://"
+    f"{configs.MONGO_USERNAME}"
+    f":{configs.MONGO_PASSWORD}"
+    f"@{configs.MONGO_HOST}"
+)
+
+
+def get_user(chat_id):
+    db = CLIENT['users']
+    collection_name = db['user_info']
+    if collection_name.count_documents({'chat_id': chat_id}) == 0:
+        return {}
+    else:
+        return collection_name.find({'chat_id': chat_id}).next()
+
+
+def add_user(chat_id, email, passphrase):
+    try:
+        new_user = copy.copy(configs.USER_FORM)
+        new_user['chat_id'] = chat_id
+        new_user['email'] = email
+        new_user['passphrase'] = passphrase
+        db = CLIENT['users']
+        collection_name = db['user_info']
+        collection_name.insert_one(new_user)
+        return True
+    except:
+        return False
+
+
+def add_conversation(chat_id, status):
+    db = CLIENT['users']
+    collection_name = db['conversation_status']
+    result = collection_name.update_one(
+        {'chat_id': chat_id},
+        {"$set": {'status': status}},
+        upsert=True
+    )
+    if result.upserted_id is None:
+        return False
+    else:
+        return True
+
+
+def get_conversation(chat_id):
+    db = CLIENT['users']
+    collection_name = db['conversation_status']
+    status = collection_name.find({'chat_id': chat_id}).next()['status']
+    return status
